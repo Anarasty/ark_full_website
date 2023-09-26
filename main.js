7a48f8ce2f31f5d959363040a30bea4f460dfaea
Anarasty <murloc.ninja16@gmail.com>
2023-09-26 16:00:38 +0300

    modal hell NEED FIXES

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log('tes1')
+
+// Получаем ссылки на элементы модального окна и кнопки "Посмотреть больше"
+const modals = document.querySelectorAll('.modal');
+const modalBackground = document.getElementById('modal-background');
+const viewButtons = document.querySelectorAll('#release_modal_btn');
+
+// Добавляем обработчики событий для кнопок "Посмотреть больше"
+viewButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        // Открываем соответствующее модальное окно и задний фон
+        modals[index].style.display = 'block';
+        modalBackground.style.display = 'block';
+        setTimeout(() => {
+            modalBackground.style.opacity = '1';
+        }, 10);
+    });
+});
+
+// Добавляем обработчики событий для кнопок "Закрыть" в модальных окнах
+modals.forEach((modal, index) => {
+    const closeButton = modal.querySelector('.close-button');
+    closeButton.addEventListener('click', () => {
+        modal.style.display = 'none';
+        modalBackground.style.opacity = '0';
+        setTimeout(() => {
+            modalBackground.style.display = 'none';
+        }, 300);
+    });
+});
